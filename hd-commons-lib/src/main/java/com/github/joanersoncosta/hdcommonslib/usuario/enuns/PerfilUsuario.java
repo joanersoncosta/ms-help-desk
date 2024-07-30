5677fa432a7f647aac483d773193886baf70ec63
Joanerson Costa <joanersonc31@gmail.com>
2024-07-30 20:17:44 -0300

    feat: modifica metodo retornaperfilusuario

--- hd-commons-lib/src/main/java/com/github/joanersoncosta/hdcommonslib/usuario/enuns/PerfilUsuario.java
+++ hd-commons-lib/src/main/java/com/github/joanersoncosta/hdcommonslib/usuario/enuns/PerfilUsuario.java
@@ -2,6 +2,10 @@ package com.github.joanersoncosta.hdcommonslib.usuario.enuns;
 
 import java.util.Arrays;
 
+import org.springframework.http.HttpStatus;
+
+import com.github.joanersoncosta.hdcommonslib.handler.APIException;
+
 public enum PerfilUsuario {
 	ADMIN("ROLE_ADMIN"), COMUM("ROLE_COMUM"), TECNICO("ROLE_TECNICO");
 
@@ -15,10 +19,10 @@ public enum PerfilUsuario {
 		return descricao;
 	}
 	
-	public static PerfilUsuario toEnum(final String descricao) {
+	public static PerfilUsuario retornaPerfilUsuario(final String descricao) {
 		return Arrays.stream(PerfilUsuario.values())
 				.filter(perfilUsuario -> perfilUsuario.getDescricao().equals(descricao))
 				.findFirst()
-				.orElseThrow(null);
+				.orElseThrow(() -> APIException.build(HttpStatus.BAD_REQUEST, "Descrição invalida!"));
 	}
 }
