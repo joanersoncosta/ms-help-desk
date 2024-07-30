ce9bc8ad981ee4f4fe576c19b58b55f78f702cd6
Joanerson Costa <joanersonc31@gmail.com>
2024-07-30 20:33:38 -0300

    feat: implementa entidade usuario

---
+++ api-usuario/src/main/java/com/github/joanersoncosta/apiusuario/usuario/domain/Usuario.java
@@ -0,0 +1,33 @@
+package com.github.joanersoncosta.apiusuario.usuario.domain;
+
+import java.util.Set;
+import java.util.UUID;
+
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.github.joanersoncosta.hdcommonslib.usuario.enuns.PerfilUsuario;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.With;
+import nonapi.io.github.classgraph.json.Id;
+
+@With
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Document(collection = "Usuario")
+public class Usuario {
+
+	@Id
+	private UUID idUsuario;
+	private String nome;
+	@Indexed(unique = true)
+	private String email;
+	private Set<PerfilUsuario> perfil;
+}
