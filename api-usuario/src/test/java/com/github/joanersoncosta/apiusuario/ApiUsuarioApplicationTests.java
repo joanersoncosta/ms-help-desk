82fd7fd357508e59220647bb972e04bb801248bd
Joanerson Costa <joanersonc31@gmail.com>
2024-07-30 18:45:20 -0300

    chore: inicia api cliente

---
+++ api-usuario/src/test/java/com/github/joanersoncosta/apiusuario/ApiUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.joanersoncosta.apiusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
