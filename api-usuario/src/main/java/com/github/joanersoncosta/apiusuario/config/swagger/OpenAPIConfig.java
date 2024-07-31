ef15b59904fb0650bf1e889ada0fbf83b322564e
Joanerson Costa <joanersonc31@gmail.com>
2024-07-31 16:42:05 -0300

    docs: adiciona documentacao openapi docs

---
+++ api-usuario/src/main/java/com/github/joanersoncosta/apiusuario/config/swagger/OpenAPIConfig.java
@@ -0,0 +1,25 @@
+package com.github.joanersoncosta.apiusuario.config.swagger;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenAPIConfig {
+
+	@Bean
+	public OpenAPI customOpenAPI(
+			@Value("${springdoc.openapi.title}") final String titlo,
+			@Value("${springdoc.openapi.description}") final String descricao,
+			@Value("${springdoc.openapi.version}") final String versao) {
+		return new OpenAPI()
+				.info(new Info()
+						.title(titlo)
+						.description(descricao)
+						.version(versao)
+						);
+		}
+}
--- api-usuario/src/main/java/com/github/joanersoncosta/apiusuario/usuario/application/api/UsuarioAPI.java
+++ api-usuario/src/main/java/com/github/joanersoncosta/apiusuario/usuario/application/api/UsuarioAPI.java
@@ -13,19 +13,46 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.github.joanersoncosta.apiusuario.usuario.application.api.request.UsuarioNovoRequest;
 import com.github.joanersoncosta.apiusuario.usuario.application.api.response.UsuarioNovoResponse;
+import com.github.joanersoncosta.hdcommonslib.handler.APIException;
 import com.github.joanersoncosta.hdcommonslib.usuario.domain.response.UsuarioResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
+@Tag(name = "UsuarioAPI", description = "Controle responsavel pelas oprações do úsuario.")
 @RestController
 @RequestMapping("/v1/usuario")
 public interface UsuarioAPI {
 	
+	@Operation(summary = "Cria novo úsuario")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "Úsuario criado"),
+			@ApiResponse(responseCode = "400", description = "Úsuario já cadastrado",
+				content = @Content(mediaType = "application/json", schema = @Schema(implementation = APIException.class))),
+			@ApiResponse(responseCode = "500", description = "Internal server error",
+				content = @Content(mediaType = "application/json", schema = @Schema(implementation = APIException.class)))
+	})
 	@ResponseStatus(code = HttpStatus.CREATED)
 	@PostMapping
 	UsuarioNovoResponse criaNovoUsuario(@RequestBody @Valid UsuarioNovoRequest usuarioRequest);
 
+	@Operation(summary = "Busca úsuario por id")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Úsuario encontrado"),
+			@ApiResponse(responseCode = "204", description = "Úsuario não encontrado",
+					content = @Content(mediaType = "application/json", schema = @Schema(implementation = APIException.class))),
+			@ApiResponse(responseCode = "500", description = "Internal server error",
+					content = @Content(mediaType = "application/json", schema = @Schema(implementation = APIException.class)))
+	})
 	@ResponseStatus(code = HttpStatus.OK)
 	@GetMapping(value  = "/{idUsuario}")
-	UsuarioResponse buscaUsuarioPorId(@PathVariable(value  = "idUsuario") UUID idUsuario);
+	UsuarioResponse buscaUsuarioPorId(
+			@Parameter(description = "Id usuario", required = true, example = "b700b45a-d1ee-41da-95bd-6d477a7c862")
+			@PathVariable(value  = "idUsuario") UUID idUsuario);
 }
