1e7413eed22926c52497c4ae911951866aa8242d
Joanerson Costa <joanersonc31@gmail.com>
2024-08-01 22:03:38 -0300

    estartei config server

---
+++ config-server/src/main/java/com/github/joanersoncosta/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.joanersoncosta.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
