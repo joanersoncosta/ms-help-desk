c95e75f23a6f9b124db23b91dcfdce9560b0c0cd
Joanerson Costa <joanersonc31@gmail.com>
2024-09-16 13:36:59 -0300

    feat: add email service

---
+++ email-service/src/main/java/com/gitthub/joanerson/emailservice/ordem/infra/OrdemListener.java
@@ -0,0 +1,9 @@
+package com.gitthub.joanerson.emailservice.ordem.infra;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+public class OrdemListener {
+}
